314f5439016d505286f22442afb74a4c0ac35b9c
DuBoris Smith <duboris.smith@hotmail.com>
2023-03-21 20:20:19 -0500

    READ.block07

---
+++ block07.js
@@ -0,0 +1,21 @@
+//This code will formulate the vault code.
+
+const vault ="You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//This code will compilate the equations into the vault combinations.
+
+const first = 2 * 5;
+const second = (4 + 6) / 2 * 8;
+const third = 3**3 + 2**4 - 4;
+const vaultCom = `Numeral code ${first} - ${second} - ${third}`;
+
+//This code will print out the combination answers to the vault.
+
+console.log(first);
+console.log(second);
+console.log(third);
+alert(vault + vaultCom)
+console.log(vault + vaultCom);
+
+/* If done correctly the following will print out
+   10 - 40 - 39*/
